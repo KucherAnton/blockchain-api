8d657b99fb7a6e15a8bc2fe6a0c8901cbe6c912d
Kucher Anton <kapetrovi4@gmail.com>
2025-06-21 23:45:07 +0300

    clearing

--- src/evm/dtos/get-block-by-height.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsInt, Min } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class GetBlockByHeightDto {
-	@Type(() => Number)
-	@IsInt()
-	@Min(0)
-	height: number;
-}
